2afbce631cce40ff9897ea43d6d3a2c0597ad31c
Tom McErlean <TM1000@live.mdx.ac.uk>
2025-12-03 00:07:52 +0000

    Alternative githubpages way

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Project_Comp/" : "/",
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
+})
